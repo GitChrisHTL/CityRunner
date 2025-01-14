ed3602368cb34b578f03c7d8661887faaf413c4c
GitChrisHTL <c.ursprung@HTBLALeonding.onmicrosoft.com>
2025-01-14 11:54:11 +0100

    some tweeks

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Retry()
+    {
+
+    }
+
+    public void Quit()
+    {
+
+    }
+}
